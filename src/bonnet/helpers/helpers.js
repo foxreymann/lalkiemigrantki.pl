7b959a6322e49aa00ef3c82704b3e8e9292bf64f
tomaszjureczko <tomasz.jureczko@gmail.com>
2015-01-06 17:52:27 +0000

    adam

--- src/bonnet/helpers/helpers.js
+++ src/bonnet/helpers/helpers.js
@@ -36,4 +36,21 @@ module.exports.register = function (Handlebars, options) {
     }
   });
 
+  Handlebars.registerHelper("debug", function(optionalValue) {
+    if (optionalValue) {
+      console.log("Value");
+      console.log("====================");
+      console.log(optionalValue);
+      console.log(optionalValue[0].pages[0]);
+      console.log(optionalValue[0].pages[1]);
+      console.log(optionalValue[0].pages[2]);
+      console.log(optionalValue[0].pages[3]);
+      return;
+    }
+    console.log("Current Context");
+    console.log("====================");
+    console.log(this);
+
+  });
+
 };
